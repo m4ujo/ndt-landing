4cf48d5ea6420d99f2c1ab4c483a628c2e0ca15b
Mauricio Castillo <mauriciocast11@outlook.com>
2022-11-01 14:46:45 -0500

    Change text of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './footer.scss'
 const Footer = () => {
   return (
     <div className='Footer'>
-      <p className='Footer-text'>© 2022 Todos Los Derechos Reservados. Welding Tech Consulting S.A.C.</p>
+      <p className='Footer-text'>© 2022 All rights reserved. Welding Tech Consulting S.A.C.</p>
     </div>
   )
 }
