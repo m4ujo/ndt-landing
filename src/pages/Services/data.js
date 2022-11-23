07617fca25223392f2fd914c54be88a761d58e3a
m4ujo <mauriciocast11@outlook.com>
2022-11-23 16:31:10 -0500

    Refactoring pages style

--- src/pages/Services/data.js
+++ src/pages/Services/data.js
@@ -1,44 +1,42 @@
 import Service1 from './../../assets/images/service1.jpg'
-import Service2 from './../../assets/images/service2.jpg'
 import Service3 from './../../assets/images/service3.jpg'
 import Service4 from './../../assets/images/service4.jpg'
 
 export const servicesImages = [
   { image: Service1, alt: '' },
-  { image: Service2, alt: '' },
   { image: Service3, alt: '' },
-  { image: Service4, alt: '' },
+  { image: Service4, alt: '' }
 ]
 
 export const servicesInfo = [
   {
     title: 'Visual Inspection',
     description:
-      'Welding Tech Consulting S.A.C, offers qualified welding inspectors and experienced technicians using state-of-the-art visual inspection equipment to meet your service needs in any industry. Visual Inspection can also be used in combination with other surface inspection methods to detect defects that are not easily identifiable to the naked eye.',
+      'Welding Tech Consulting S.A.C, offers qualified welding inspectors and experienced technicians using state-of-the-art visual inspection equipment to meet your service needs in any industry. Visual Inspection can also be used in combination with other surface inspection methods to detect defects that are not easily identifiable to the naked eye.'
   },
   {
     title: 'Dye penetrant inspection',
     description:
-      'Penetrant testing is a step ahead of visual inspection and offers many advantages, such as speed, large area coverage and cost-effectiveness, given the efficiency of the method to detect open surface flaws leading to fracture in ferrous and non-ferrous materials.',
+      'Penetrant testing is a step ahead of visual inspection and offers many advantages, such as speed, large area coverage and cost-effectiveness, given the efficiency of the method to detect open surface flaws leading to fracture in ferrous and non-ferrous materials.'
   },
   {
     title: 'Magnetic particle inspection',
     description:
-      'Magnetic particle inspection is considered to be one of the most sensitive ways to detect surface or near-surface flaws in ferrous materials. It is commonly used to inspect plates prior to welding, in the process of inspection of each pass or layer of weld, post-weld evaluation and to inspect weld repairs.',
+      'Magnetic particle inspection is considered to be one of the most sensitive ways to detect surface or near-surface flaws in ferrous materials. It is commonly used to inspect plates prior to welding, in the process of inspection of each pass or layer of weld, post-weld evaluation and to inspect weld repairs.'
   },
   {
     title: 'Ultrasonic testing',
     description:
-      'Frequently used for thickness gauging, corrosion inspection and full penetration examination of weld grooves. Ultrasonic inspection is a highly reliable detection method that proves to be very efficient in the field due to the portability of the equipment and the ability to evaluate and record results at the time of inspection.',
+      'Frequently used for thickness gauging, corrosion inspection and full penetration examination of weld grooves. Ultrasonic inspection is a highly reliable detection method that proves to be very efficient in the field due to the portability of the equipment and the ability to evaluate and record results at the time of inspection.'
   },
   {
     title: 'NDT Services',
     description:
-      'They include, but are not limited to the following: Materials Inspection, Welding Inspection ─ Fabrication / Repair, Mold Inspection Thickness Measurement / Corrosion Mapping, Ball Mill Inspection (Ball Mills), Tank Inspection, Equipment Inspection',
+      'They include, but are not limited to the following: Materials Inspection, Welding Inspection ─ Fabrication / Repair, Mold Inspection Thickness Measurement / Corrosion Mapping, Ball Mill Inspection (Ball Mills), Tank Inspection, Equipment Inspection'
   },
   {
     title: 'Services Associates',
     description:
-      'Plant Inspection, Quality Assurance,Quality Control, Welding Inspection, Engineering & Maintenance Solutions, Plant Audit & Assessment, Risk Assessment, Good Practices Inspection',
-  },
+      'Plant Inspection, Quality Assurance,Quality Control, Welding Inspection, Engineering & Maintenance Solutions, Plant Audit & Assessment, Risk Assessment, Good Practices Inspection'
+  }
 ]
